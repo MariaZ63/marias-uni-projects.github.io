9f9ff93e74b8b893d0bac0410805bbdaa2c501f4
Maria Zieglmeier <maria-zieglmeier@web.de>
2024-03-07 18:33:43 +0100

    trying to resolve deploy problem

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/mariaz63.github.io/marias-uni-projects.github.io/',
+  base:'/marias-uni-projects.github.io/',
   plugins: [react()],
   
 })
