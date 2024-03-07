0d43469e51435d343884903904ffb2e919722314
Maria Zieglmeier <maria-zieglmeier@web.de>
2024-03-07 17:42:05 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/marias-uni-projects.github.io/',
   plugins: [react()],
-  build: {
+  /* build: {
     outDir: 'build',
-    assetsInclude: ['**/*.png','**/*.JPG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], 
-  }
+  } */
 })
