57479a2a7712303e26bb7b742347c00ddf94963f
Maria Zieglmeier <maria-zieglmeier@web.de>
2024-03-16 14:29:34 +0100

    successfully include react project

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => `
+  <footer class="footer d-flex flex-wrap justify-content-center mt-auto py-3 border-top">
+    <div class="container text-center">
+      <span class="text-muted">2024 Maria Zieglmeier</span>
+    </div>
+  </footer>
+`;
+
+export default Footer;
--- src/projects/iBooks/src/index.jsx
+++ src/projects/iBooks/src/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import "./assets/css/bootstrap.default.css";
 import "./assets/css/style.css";
 import Root from "./routes/Root.jsx";
@@ -8,7 +12,7 @@ import BookDetail from "./routes/BookDetail.jsx";
 import AddBook from "./routes/AddBook.jsx";
 import BooksList from "./routes/BooksList.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
-import Store from "./model/Store.js";
+//import Store from "./model/Store.js";
 import NotFound from "./routes/NotFound.jsx";
 import NoBookDetail from "./routes/NoBookDetail.jsx";
 
@@ -22,14 +26,13 @@ import NoBookDetail from "./routes/NoBookDetail.jsx";
  */
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/src/projects/iBooks/dist/index.html",
     element: <Root />,
     errorElement: <ErrorPage />,
 
     children: [
-      
       {
-        path: "/",
+        path: "",
         element: <Navigate to="addBooks" replace={true} />,
       },
       {
@@ -56,9 +59,51 @@ const router = createBrowserRouter([
   },
 ]);
 
-
 // One-time connection to Firestore database
-Store.connectDataBase()
+//Store.connectDataBase();
+const testData = [
+  {
+    author: "Max",
+    title: "Hello World",
+    isbn: "123-4567-89-2",
+    description:
+      "First Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    stars: 1,
+  },
+  {
+    author: "Mike",
+    title: "Crazy Game",
+    isbn: "123-4567-89-X",
+    description:
+      "Third Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    stars: 5,
+  },
+  {
+    author: "Fritz",
+    title: "Hello Dave",
+    isbn: "123-4567-89-5",
+    description:
+      "Second Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    stars: 3,
+  },
+  {
+    author: "Adam",
+    title: "Hello Adam",
+    isbn: "123-4567-87-8",
+    description:
+      "Second Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    stars: 2,
+  },
+  {
+    author: "Zlatan",
+    title: "Hello Zlatan",
+    isbn: "123-4567-87-7",
+    description:
+      "Second Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    stars: 1,
+  },
+];
+localStorage.setItem("books", JSON.stringify(testData));
 
 /**
  * main root for React
