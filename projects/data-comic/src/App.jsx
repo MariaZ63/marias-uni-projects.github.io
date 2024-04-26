cf188c60a47f4dbe869778cc5dd8fb5f62843678
Maria Zieglmeier <maria-zieglmeier@web.de>
2024-04-26 09:54:11 +0200

    add thesis project

---
+++ projects/data-comic/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Panel01 from "./Panel01";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <p>
+        <Panel01 />
+      </p>
+    </>
+  );
+};
+
+export default App;
